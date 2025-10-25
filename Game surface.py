590ec537f2d75ae1fd5c6241c2c38fe30a9e0f29
salazarlhorddonnie <salazarlhorddonnie@gmail.com>
2025-10-25 17:44:46 +0800

    made play button plan to create states
    play button is now functioning and now I added new file to create game states

--- Game surface.py
+++
@@ -1,9 +0,0 @@
-import pygame
-
-pygame.init()
-
-class buttons():
-	def __init__(self, play, settings, back):
-		self.play =
-		self.settings =
- 		self.back =  
--- Main.py
+++ Main.py
@@ -1,20 +1,32 @@
 import pygame as py
 from sys import exit
+from Game_surface import Button
 
 def main():
     py.init()
     screen = py.display.set_mode((0, 0), py.FULLSCREEN)
-    py.display.set_caption("Sequnce")
+    py.display.set_caption("Sequence")
 
     clock = py.time.Clock()
-    running = True
 
+    font = py.font.Font(None, 80)
+
+    play_button = Button("PLAY", font, (0, 0, 0), (640, 360), 300, 120, (255, 255, 255))
+
+    running = True
     while running:
         for event in py.event.get():
             if event.type == py.QUIT:
                 running = False
+            if event.type == py.KEYDOWN and event.key == py.K_ESCAPE:
+                running = False
+
+            if event.type == py.MOUSEBUTTONDOWN:
+                if play_button.is_clicked(event.pos):
+                    print("Play button clicked!")
 
         screen.fill((30, 30, 30))
+        play_button.draw(screen)
         py.display.flip()
         clock.tick(60)
 
@@ -22,4 +34,4 @@ def main():
     exit()
 
 if __name__ == '__main__':
-	main()
+    main()
